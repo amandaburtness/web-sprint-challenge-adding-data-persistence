474438eb6eed7fcf5b26c20edebd4118b1dcea0e
Amanda Burtness <amandaburtness@gmail.com>
2022-03-06 22:23:18 -0600

    mvp

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,28 @@
 // build your `Project` model here
+const db = requier('../../data/dbConfig')
+
+const getProject = async () => {
+  const results = await db('projects as p')
+    .select('p.project_id', 'p.project_name', 'p.project_description', 'p.project_completed')
+
+    const projects = []
+
+    results.forEach(result => {
+      projects.push({
+        project_id: result.project_id,
+        project_name: result.project_name,
+        project_description: result.project_description,
+        project_completed: Boolean(result.project_completed)
+      })
+    })
+  return projects
+}
+
+const addProject = (project) => {
+  return db('projects').insert(project)
+} 
+
+module.exports = {
+  getProject,
+  addProject
+}
